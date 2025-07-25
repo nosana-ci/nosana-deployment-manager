ba465dcfccc2af66b0947474c2a398589f5b08a9
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-07-25 14:23:09 +0000

    revert document validation due to unsupported engine

--- src/connection/schemas/createEventsCollectionSchema.ts
+++ src/connection/schemas/createEventsCollectionSchema.ts
@@ -8,40 +8,40 @@ export async function createEventsCollectionSchema(
     await db.createCollection("events");
   }
 
-  await db.command({
-    collMod: "events",
-    validator: {
-      $jsonSchema: {
-        bsonType: "object",
-        required: ["category", "deploymentId", "type", "message", "created_at"],
-        properties: {
-          category: {
-            bsonType: "string",
-            description: "Type of event.",
-          },
-          deploymentId: {
-            bsonType: "string",
-            description: "Deployment source address.",
-          },
-          type: {
-            bsonType: "string",
-            description: "Error type.",
-          },
-          message: {
-            bsonType: "string",
-            description: "Error message.",
-          },
-          tx: {
-            bsonType: "string",
-            description: "Solana transaction id of job list.",
-          },
-          created_at: {
-            bsonType: "date",
-            description: "Creation date.",
-          },
-        },
-      },
-    },
-    validationLevel: "moderate",
-  });
+  // await db.command({
+  //   collMod: "events",
+  //   validator: {
+  //     $jsonSchema: {
+  //       bsonType: "object",
+  //       required: ["category", "deploymentId", "type", "message", "created_at"],
+  //       properties: {
+  //         category: {
+  //           bsonType: "string",
+  //           description: "Type of event.",
+  //         },
+  //         deploymentId: {
+  //           bsonType: "string",
+  //           description: "Deployment source address.",
+  //         },
+  //         type: {
+  //           bsonType: "string",
+  //           description: "Error type.",
+  //         },
+  //         message: {
+  //           bsonType: "string",
+  //           description: "Error message.",
+  //         },
+  //         tx: {
+  //           bsonType: "string",
+  //           description: "Solana transaction id of job list.",
+  //         },
+  //         created_at: {
+  //           bsonType: "date",
+  //           description: "Creation date.",
+  //         },
+  //       },
+  //     },
+  //   },
+  //   validationLevel: "moderate",
+  // });
 }

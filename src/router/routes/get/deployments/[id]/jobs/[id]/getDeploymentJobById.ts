33df8d4076f4e0de0c081bdb68880331e73d7faf
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-10-29 11:30:55 +0000

    feat: /deployments/{deployment}/jobs/{job}

--- src/router/routes/get/deployments/[id]/jobs/[id]/getDeploymentJobById.ts
+++ src/router/routes/get/deployments/[id]/jobs/[id]/getDeploymentJobById.ts
@@ -1,7 +1,52 @@
 import type { RouteHandler } from "fastify";
 
-export const getDeploymentJobByIdHandler: RouteHandler<{
+import { getSdk } from "../../../../../../../sdk/index.js";
 
-}> = async () => {
-  return "hello";
+export const getDeploymentJobByIdHandler: RouteHandler<{
+  Params: { deployment: string; job: string };
+}> = async (req, res) => {
+  const sdk = getSdk()
+  const { job: jobId } = req.params;
+  const deployment = res.locals.deployment!
+  const { results: resultsCollection } = res.locals.db
+
+  const job = deployment.jobs.find((job) => job.job === jobId);
+
+  if (!job) {
+    res.status(404).send({
+      error: "Job not found",
+    });
+    return;
+  }
+
+  const revision = deployment.revisions.find((rev) => rev.revision === job.revision);
+
+  if (!revision) {
+    res.status(500).send({
+      error: "Job revision not found",
+    });
+    return;
+  }
+
+  let results = await resultsCollection.findOne({
+    job: job.job,
+  });
+
+  const onChain = await sdk.jobs.get(jobId);
+
+  if (results === null && onChain.state === "COMPLETED") {
+    results = await sdk.ipfs.retrieve(onChain.ipfsResult);
+  }
+
+  res.status(200).send({
+    price: onChain.price,
+    market: onChain.market.toString(),
+    state: onChain.state,
+    node: onChain.node,
+    confidential: deployment.confidential,
+    revision: job.revision,
+    jobDefinition: revision.job_definition,
+    results,
+    created_at: job.created_at.toISOString(),
+  });
 } 

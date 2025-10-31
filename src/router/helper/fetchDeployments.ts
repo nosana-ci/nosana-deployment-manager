c1c45be6db4eb423abf52d7e5e0489c9c01d70b1
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-10-31 09:23:16 +0000

    convert mongo sort to js sort

--- src/router/helper/fetchDeployments.ts
+++ src/router/helper/fetchDeployments.ts
@@ -1,6 +1,9 @@
 import {
   DeploymentAggregation,
   DeploymentCollection,
+  EventDocument,
+  JobsDocument,
+  RevisionDocument,
 } from "../../types/index.js";
 
 export async function fetchDeployments(
@@ -46,13 +49,6 @@ export async function fetchDeployments(
       },
     },
     {
-      $addFields: {
-        events: { $sortArray: { input: "$events", sortBy: { created_at: -1 } } },
-        jobs: { $sortArray: { input: "$jobs", sortBy: { created_at: -1 } } },
-        revisions: { $sortArray: { input: "$revisions", sortBy: { revision: -1 } } },
-      },
-    },
-    {
       $project: {
         _id: false,
         "revisions._id": false,
@@ -63,6 +59,21 @@ export async function fetchDeployments(
     },
   ];
 
-  const deployment = await deployments.aggregate(pipeline).toArray();
-  return deployment as DeploymentAggregation[];
+  const deployment = await deployments.aggregate<DeploymentAggregation>(pipeline).toArray();
+
+  return deployment.map((deployment) => {
+    const sortByDateDesc = (a: EventDocument | JobsDocument, b: EventDocument | JobsDocument) =>
+      new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
+
+    const sortRevisionsDesc = (a: RevisionDocument, b: RevisionDocument) => {
+      const ra = typeof a?.revision === "number" ? a.revision : parseInt(String(a?.revision || ""), 10) || 0;
+      const rb = typeof b?.revision === "number" ? b.revision : parseInt(String(b?.revision || ""), 10) || 0;
+      return rb - ra;
+    };
+
+    deployment.events.sort(sortByDateDesc);
+    deployment.jobs.sort(sortByDateDesc);
+    deployment.revisions.sort(sortRevisionsDesc);
+    return deployment;
+  });
 }

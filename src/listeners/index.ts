f2b99f0ae95fe9a944ed12e7555b833ca1aebbb8
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-12-02 08:06:35 +0000

    fix imports

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -1,11 +1,11 @@
 import { Db } from "mongodb";
 
-import { startDeploymentListener } from "./deployments/index.js";
-import { startTaskListener } from "../tasks/index.js";
+import { startDeploymentCollectionListener } from "./deployments/index.js";
+import { startTaskCollectionListener } from "../tasks/index.js";
 import { startJobAccountsListeners } from "./accounts/index.js";
 
 export function startDeploymentManagerListeners(db: Db) {
-  startDeploymentListener(db);
-  startTaskListener(db);
+  startDeploymentCollectionListener(db);
+  startTaskCollectionListener(db);
   startJobAccountsListeners(db);
 }

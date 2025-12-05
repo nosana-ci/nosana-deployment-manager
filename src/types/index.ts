f9d3f4b262ab79ed04f21aee01cd94486b6153b0
Matthew Hammond <ultramatt96@hotmail.co.uk>
2025-12-05 10:06:44 +0000

    chore: remove web3.js

--- src/types/index.ts
+++ src/types/index.ts
@@ -104,7 +104,7 @@ export type VaultDocument = {
   vault: string;
   vault_key: string;
   owner: string;
-  created_at: Date;
+  creategd_at: Date;
 };
 
 export type VaultCollection = Collection<VaultDocument>;

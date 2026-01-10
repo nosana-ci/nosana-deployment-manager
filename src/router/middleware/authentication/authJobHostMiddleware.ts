518dca7258b399e4351eb7f0c342b2d3bd5dbf20
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-01-10 02:30:52 +0000

    feat: authJobHostMiddleware only 403 if queued

--- src/router/middleware/authentication/authJobHostMiddleware.ts
+++ src/router/middleware/authentication/authJobHostMiddleware.ts
@@ -39,7 +39,7 @@ export const authJobHostMiddleware: RouteHandler<{
         return;
       }
 
-      if (![JobState.RUNNING, JobState.STOPPED].includes(job.state) && job.node.toString() !== '11111111111111111111111111111111') {
+      if (job.state === JobState.QUEUED && job.node.toString() !== '11111111111111111111111111111111') {
         res.status(403).send("Job must be running");
         return;
       }

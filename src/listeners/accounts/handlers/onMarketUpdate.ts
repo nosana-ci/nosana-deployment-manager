97557c176bf2b0dd3fe38251eca25ecf94f5beaa
Miguel Ferreira <miguelferreira@me.com>
2026-01-15 12:05:21 +0100

    Address diff between original branch and result after rebase
    Rebase was done to squash 2 fixup! commits. After that there was a diff
    between the resulting brnach and the original at
    89ea42643a4902e9a4cfe8e907373bbabc50b68b.
    This commit addresses that diff.
    
    The diff is caused when rebasing removes the merge commits that were
    present in the original branch.

--- src/listeners/accounts/handlers/onMarketUpdate.ts
+++ src/listeners/accounts/handlers/onMarketUpdate.ts
@@ -7,7 +7,7 @@ import { checkJobExists, findQueuedJobsByMarket } from "../helpers/index.js";
 
 import { type JobsDocument, JobState } from "../../../types/index.js";
 
-const isJobQueuedAndNotEmpty = (market: Market) => market.queueType === MarketQueueType.JOB_QUEUE || !market.queue.length;
+const isJobQueueAndNotEmpty = (market: Market) => market.queueType === MarketQueueType.JOB_QUEUE && market.queue.length > 0;
 
 export async function onMarketUpdate(
   db: Db,
@@ -23,7 +23,7 @@ export async function onMarketUpdate(
 
   // Check if all our queued jobs in this market are actually still queued on-chain
   // A delist instruction can remove the job from the on-chain queue
-  if (isJobQueuedAndNotEmpty(marketAccount)) {
+  if (isJobQueueAndNotEmpty(marketAccount)) {
     for (const address of marketAccount.queue) {
       if (queuedJobsFromDB.has(address.toString())) {
         queuedJobsFromDB.delete(address.toString());
@@ -39,15 +39,14 @@ export async function onMarketUpdate(
         }
       }
 
-      await jobsCollection.updateMany(
-        { job: { $in: Array.from(queuedJobsFromDB) } },
-        { $set: { state: JobState.STOPPED } }
-      );
-
+      if (queuedJobsFromDB.size > 0) {
+        await jobsCollection.updateMany(
+          { job: { $in: Array.from(queuedJobsFromDB) } },
+          { $set: { state: JobState.STOPPED } }
+        );
+      }
     } catch (e: unknown) {
       console.log('Could not check job account on-chain', e);
     }
-
   }
-
 }

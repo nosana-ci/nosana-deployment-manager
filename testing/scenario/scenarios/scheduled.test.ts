f5293fac774de963ccfeb6812b6bfd0b2507e3f8
Miguel Ferreira <miguelferreira@me.com>
2026-01-16 20:18:58 +0100

    Add scheduled transaction-error to scenario

--- testing/scenario/scenarios/scheduled.test.ts
+++ testing/scenario/scenarios/scheduled.test.ts
@@ -1,7 +1,8 @@
 import { describe } from 'vitest';
 
-describe('Simple Strategy Scenarios', async () => {
+describe('Scheduled Strategy Scenarios', async () => {
   await import('./scheduled/basic-flow.test.js');
   await import('./scheduled/multiple-replicas.test.js');
   await import('./scheduled/schedule-repeats.test.js');
+  await import('./scheduled/transaction-error.test.js');
 });

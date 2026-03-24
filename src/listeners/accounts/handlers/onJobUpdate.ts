28c213c6a8f13c72f6b6b2d8f7c8b1b244fb9409
Matthew Hammond <ultramatt96@hotmail.co.uk>
2026-03-24 10:44:18 +0000

    feat: return node form deployment job endpoints

--- src/listeners/accounts/handlers/onJobUpdate.ts
+++ src/listeners/accounts/handlers/onJobUpdate.ts
@@ -6,19 +6,26 @@ import { NosanaCollections } from "../../../definitions/collection.js";
 
 import { type JobsDocument, JobState } from "../../../types/index.js";
 
+function guardAgainstDefaultNodeAddress(node: string | null): string | null {
+  if (node === "11111111111111111111111111111111") {
+    return null;
+  }
+  return node;
+}
+
 export async function onJobUpdate(
   db: Db,
   jobData: Job
 ): Promise<void> {
   const jobsCollection = db.collection<JobsDocument>(NosanaCollections.JOBS);
-  const { address, state, timeStart } = jobData;
+  const { address, node, state, timeStart } = jobData;
 
   await jobsCollection.updateOne(
     {
       job: address.toString(),
       state: { $nin: [JobState.COMPLETED, JobState.STOPPED] } // required to ensure states don't regress
     },
-    { $set: { state: convertJobState(state), time_start: Number(timeStart) } },
+    { $set: { state: convertJobState(state), time_start: Number(timeStart), node: guardAgainstDefaultNodeAddress(node) } },
     { upsert: false }
   );
 }

7b909409a3b72968edc19d172190fdf70d52f504
Miguel Ferreira <miguelferreira@me.com>
2026-04-06 08:51:32 +0200

    fix: script to verify dB restore by settign auth mechanism in connection string

--- scripts/db-migration/verify-restore.sh
+++ scripts/db-migration/verify-restore.sh
@@ -3,7 +3,7 @@
 set -e
 
 DB_NAME="${DST_DBNAME}"
-TLS_OPTS="tls=true&tlsCAFile=/data/global-bundle.pem&replicaSet=rs0"
+TLS_OPTS="tls=true&tlsCAFile=/data/global-bundle.pem&replicaSet=rs0&authMechanism=SCRAM-SHA-1"
 
 echo "=== DESTINATION cluster ==="
 mongosh "mongodb://${DST_USERNAME}:${DST_PASSWORD}@${DST_HOST}:${DST_PORT}/${DB_NAME}?${TLS_OPTS}" --quiet \
